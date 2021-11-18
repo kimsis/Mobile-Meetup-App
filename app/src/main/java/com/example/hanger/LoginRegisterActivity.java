72106124a8e892d7faa5f0bb0b186cb1543f27b7
Dimitar Hristov <mimaqm@gmail.com>
2021-11-18 13:04:21 +0100

    Made some file changes

---
+++ app/src/main/java/com/example/hanger/LoginRegisterActivity.java
@@ -0,0 +1,27 @@
+package com.example.hanger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_register);
+
+        Button signButton = findViewById(R.id.btn_sign_in);
+
+        signButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(mainIntent);
+            }
+        });
+    }
+}
