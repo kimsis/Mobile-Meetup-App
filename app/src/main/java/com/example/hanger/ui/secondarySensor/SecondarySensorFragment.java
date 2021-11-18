490222343f0bbc72081725f3e47368fd999a06ae
Dimitar Hristov <mimaqm@gmail.com>
2021-11-18 14:32:44 +0100

    Some UI fixes

--- app/src/main/java/com/example/hanger/ui/secondarySensor/SecondarySensorFragment.java
+++ app/src/main/java/com/example/hanger/ui/secondarySensor/SecondarySensorFragment.java
@@ -12,7 +12,6 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
