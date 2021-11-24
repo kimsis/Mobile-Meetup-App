48268059b1d6e0b74fdedda473eef1d82d89c7f3
Dimitar Hristov <mimaqm@gmail.com>
2021-11-24 23:45:20 +0100

    Added RequestsFragment

---
+++ app/src/main/java/com/example/hanger/ui/requests/RequestsFragment.java
@@ -0,0 +1,52 @@
+package com.example.hanger.ui.requests;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.hanger.R;
+import com.example.hanger.databinding.FragmentRequestsBinding;
+import com.example.hanger.databinding.FragmentSecondarySensorBinding;
+
+public class RequestsFragment extends Fragment {
+
+    private FragmentRequestsBinding binding;
+
+    @Override
+    public final void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        RequestsViewModel requestsViewModel = new ViewModelProvider(this).get(RequestsViewModel.class);
+        binding = FragmentRequestsBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
